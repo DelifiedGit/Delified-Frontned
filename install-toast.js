d28dec5dc91f5f5f88eabe2721652a8b5e7c9cea
DelifiedGit <delified06@gmail.com>
2024-11-25 00:45:43 +0530

    Add files via upload

---
+++ install-toast.js
@@ -0,0 +1,7 @@
+console.log("Installing @radix-ui/react-toast...")
+console.log("Run the following command in your terminal:")
+console.log("npm install @radix-ui/react-toast")
+
+// Simulating the installation process
+console.log("Installing...")
+console.log("Package @radix-ui/react-toast has been successfully installed.")
